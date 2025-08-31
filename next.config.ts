bad11efd12bbbd78aec5ec43e4a969eb998acd11
dnisenboym <d.nisenboym@gmail.com>
2025-08-31 12:41:21 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { domains: ["images.unsplash.com"] },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" }
+    ],
+  },
 };
 
 export default nextConfig;
