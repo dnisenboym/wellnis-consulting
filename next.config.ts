1a3bfeadd27c43e7ae2d5fea2dae54c1de90eef5
dnisenboym <d.nisenboym@gmail.com>
2025-08-31 12:38:20 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["images.unsplash.com"],
-  },
+  images: { domains: ["images.unsplash.com"] },
 };
 
 export default nextConfig;
